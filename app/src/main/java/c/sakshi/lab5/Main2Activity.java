d777908cc3b91a0f4647545f47e22b2f2a5ac3eb
Jun Yu (Jimmy) Ma <Jimmy.ma3615@gmail.com>
2020-02-27 14:25:26 -0600

    Milestone 1 Complete

---
+++ app/src/main/java/c/sakshi/lab5/Main2Activity.java
@@ -0,0 +1,50 @@
+package c.sakshi.lab5;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView WelcomeMsg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        String login = sharedPreferences.getString("username", "");
+        WelcomeMsg = findViewById(R.id.textView);
+        WelcomeMsg.setText("Welcome " + login + "!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.item1:
+                Intent intent = new Intent(this, MainActivity.class);
+                SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+                sharedPreferences.edit().remove("username").apply();
+                startActivity(intent);
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,13 +1,43 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        String usernameKey = "username";
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        if (!sharedPreferences.getString(usernameKey, "").equals("")){
+            String login = sharedPreferences.getString(usernameKey, "");
+            Intent intent = new Intent(this, Main2Activity.class);
+            intent.putExtra("message", login);
+            startActivity(intent);
+        }
+        else{
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+    public void clickFunction (View view){
+        EditText myText = (EditText) findViewById(R.id.login);
+        String str = myText.getText().toString();
+        goToActivity2(str);
+    }
+    public void goToActivity2 (String s){
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", s);
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username", s).apply();
+        startActivity(intent);
     }
 }
